fe28c45d94549dced40ea9a675f1bee33f5527d6
RCherezbiev <149434791+RCherezbiev@users.noreply.github.com>
2024-03-09 19:36:15 +0300

    Initial Commit

---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Rustam Cherezbiev on 09.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
